f7852b8585c0667ff57f3e169a2142beb3062519
OcelotEmpire <hobbz.pi@gmail.com>
2023-11-17 00:12:47 -0600

    fix?

--- physics/waves/wave_demo_1.js
+++ physics/waves/wave_demo_1.js
@@ -3,8 +3,16 @@
  * Missouri S&T Physics Department
  */
 
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+const stepBackButton = document.getElementById('step-back');
+const stepForwardButton = document.getElementById('step-forward');
+const resetButton = document.getElementById('reset');
+
 const canvas = document.getElementById('display');
 const ctx = canvas.getContext('2d');
+var running = false;
+var time = 0.0;
 
 function clear(){
     ctx.fillStyle = '#f1f1f1';
@@ -15,3 +23,37 @@ function clear(){
 function draw(){
     clear();
 }
+
+var run = function() {
+    update();
+    draw();
+    time += 0.1
+    if(running){
+        setTimeout(run, 100);
+    }
+}
+
+function start(){
+    if(!running){
+        startButton.disabled = true;
+        pauseButton.diabled = false;
+        stepBackButton.disabled = true;
+        stepForwardButton.disabled = true;
+        running = true;
+        console.log('starting...');
+    }
+}
+function pause(){
+    if(running){
+        startButton.disabled = false;
+        pauseButton.disabled = true;
+        stepBackButton.disabled = true;
+        stepForwardButton.disabled = true;
+        running = false;
+        console.log('stopping...')
+    }
+
+}
+startButton.onclick = start;
+pauseButton.onclick = pause;
+pauseButton.disabled = true;
